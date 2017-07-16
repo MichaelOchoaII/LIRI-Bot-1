63b11d44b87634aa048b17eabd149796e8cc15c9
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-07-16 15:16:41 -0500

    master - promises added to each function

--- liri.js
+++ liri.js
@@ -15,112 +15,119 @@ var liri = {
     if(process.argv.length >2){
       this.args = process.argv.slice(3, process.argv.length).join(" ");
     }
-    this.runCommand(this.command, this.args, false, this.printOutput).then(function(output){
-      console.log(output);
-    });
+    this.runCommand(this.command, this.args, false, this.printOutput);
 
   },
 
-  printOutput: function(){
+  printOutput: function(output){
     console.log(output);
   },
 
   runCommand: function(command, args, recurse, callback){
     console.log(command);
-    return new Promise(function(resolve, reject){
       if(command === "my-tweets"){
-        results = liri.myTweets();
+        liri.myTweets().then(function(results){
+          callback(results);
+        });
       }
       if(command === "spotify-this-song"){
-        results = liri.spotifyThisSong(args);
+        liri.spotifyThisSong(args).then(function(results){
+          callback(results);
+        });
       }
       if(command === "movie-this"){
-        results = liri.movieThis(args.replace(" ", "+"));
+        liri.movieThis(args.replace(" ", "+")).then(function(results){
+          callback(results);
+        });
       }
       if(command === "do-what-it-says"){
         if(!recurse){
-          results = liri.doWhatItSays();
+          liri.doWhatItSays();
         }
       }
-      resolve(results);
-    });
+
   },
 
   myTweets: function(){
     // console.log("Tweets");
-    var output = "";
-    var client = new twitter(this.keys.twitterKeys);
-    client.get('statuses/user_timeline', function(error, tweets, response) {
-      if(error){
-        console.log(error);
-        return;
-      }
-      for(var t = 0; t<Math.min(20, tweets.length);t++){
-        // console.log(tweets[t].created_at+"\n"+tweets[t].text+"\n");
-        output += tweets[t].created_at+"\n"+tweets[t].text+"\n";
-      }
-      console.log("TEST"+output)
-      return output;
+    return new Promise(function(resolve, reject){
+      var output = "";
+      var client = new twitter(liri.keys.twitterKeys);
+      client.get('statuses/user_timeline', function(error, tweets, response) {
+        if(error){
+          console.log(error);
+          return;
+        }
+        for(var t = 0; t<Math.min(20, tweets.length);t++){
+          // console.log(tweets[t].created_at+"\n"+tweets[t].text+"\n");
+          output += tweets[t].created_at+"\n"+tweets[t].text+"\n";
+        }
+        resolve(output);
+      });
     });
   },
 
   spotifyThisSong: function(song){
     // console.log("spotify");
-    console.log(song);
+    // console.log(song);
     // var output = "";
-    var spotify = new spotifyApi(this.keys.spotifyKeys);
-    spotify.search({ type: 'track', query: song })
-    .then(function(response) {
-      // console.log(response.tracks.items[0]);
-      var resultTrack = response.tracks.items[0];
-      var artistlist = [];
-      for(var a in resultTrack.artists){
-        artistlist.push(resultTrack.artists[a].name);
-      }
-      if(artistlist.length > 0){
-        artistlist = artistlist.join(", ");
-      }
+    return new Promise(function(resolve, reject){
+      var spotify = new spotifyApi(liri.keys.spotifyKeys);
+      spotify.search({ type: 'track', query: song })
+      .then(function(response) {
+        // console.log(response.tracks.items[0]);
+        var resultTrack = response.tracks.items[0];
+        var artistlist = [];
+        for(var a in resultTrack.artists){
+          artistlist.push(resultTrack.artists[a].name);
+        }
+        if(artistlist.length > 0){
+          artistlist = artistlist.join(", ");
+        }
 
-      // console.log(artistlist);
-      // console.log(resultTrack.name);
-      // console.log(resultTrack.preview_url);
-      // console.log(resultTrack.album.name);
-      return artistlist + "\n" + resultTrack.name + "\n" + resultTrack.preview_url + "\n" + resultTrack.album.name;
-    })
-    .catch(function(err) {
-      console.log(error);
-    });
+        // console.log(artistlist);
+        // console.log(resultTrack.name);
+        // console.log(resultTrack.preview_url);
+        // console.log(resultTrack.album.name);
+        resolve(artistlist + "\n" + resultTrack.name + "\n" + resultTrack.preview_url + "\n" + resultTrack.album.name);
+      })
+      .catch(function(err) {
+        console.log(error);
+      });
+  });
   },
 
   movieThis: function(movie){
     // console.log("movie");
-    var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
-    request(queryUrl, function(error, response, body){
-      if(!error && response.statusCode === 200){
-        var movieinfo = JSON.parse(body);
-        console.log("Title: "+movieinfo.Title);
-        console.log("Year: "+movieinfo.Year);
-        var imdbrating, rtrating;
-        for(var r in movieinfo.Ratings){
-          if(r.Source === "Internet Movie Database"){
-            imdbrating = r.Value;
+    return new Promise(function(resolve, reject){
+      var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
+      request(queryUrl, function(error, response, body){
+        if(!error && response.statusCode === 200){
+          var movieinfo = JSON.parse(body);
+          // console.log("Title: "+movieinfo.Title);
+          // console.log("Year: "+movieinfo.Year);
+          var imdbrating, rtrating;
+          for(var r in movieinfo.Ratings){
+            if(r.Source === "Internet Movie Database"){
+              imdbrating = r.Value;
+            }
+            else if(r.Source === "Rotten Tomatoes"){
+              rtrating = r.Value;
+            }
           }
-          else if(r.Source === "Rotten Tomatoes"){
-            rtrating = r.Value;
-          }
+          // console.log("IMDB Rating: "+imdbrating);
+          // console.log("Rotten Tomatoes Rating: "+rtrating);
+          // console.log("Country: "+movieinfo.Country);
+          // console.log("Language: "+movieinfo.Language);
+          // console.log("Plot: "+movieinfo.Plot);
+          // console.log("Actors: "+movieinfo.Actors);
+          resolve("Title: "+movieinfo.Title+"\n"+"Year: "+movieinfo.Year+"\n"+"IMDB Rating: "+imdbrating+"\nRotten Tomatoes Rating: "+rtrating+"\nCountry: "+movieinfo.Country+"\nLanguage: "+movieinfo.Language+"\nPlot: "+movieinfo.Plot+"\nActors: "+movieinfo.Actors);
         }
-        // console.log("IMDB Rating: "+imdbrating);
-        // console.log("Rotten Tomatoes Rating: "+rtrating);
-        // console.log("Country: "+movieinfo.Country);
-        // console.log("Language: "+movieinfo.Language);
-        // console.log("Plot: "+movieinfo.Plot);
-        // console.log("Actors: "+movieinfo.Actors);
-        return "IMDB Rating: "+imdbrating+"\nRotten Tomatoes Rating: "+rtrating+"\nCountry: "+movieinfo.Country+"\nLanguage: "+movieinfo.Language+"\nPlot: "+movieinfo.Plot+"\nActors: "+movieinfo.Actors;
-      }
-      else{
-        console.log(error);
-      }
-    });
+        else{
+          console.log(error);
+        }
+      });
+  });
   },
 
   doWhatItSays: function(){
@@ -134,10 +141,12 @@ var liri = {
       var commandlist = data.split("\n");
       for(var c in commandlist){
         var currentCommand = commandlist[c].trim().split(",");
-        liri.runCommand(currentCommand[0], currentCommand[1], true, this.printOutput);
+        liri.runCommand(currentCommand[0], currentCommand[1], true, liri.printOutput);
       }
     });
   }
+
+
 };
 
 liri.run();
