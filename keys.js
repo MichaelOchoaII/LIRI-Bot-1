38273f2e9e1fd98a102ce4573d76ea1180e2c7f9
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-07-15 21:48:20 -0500

    master - added functionality for my-tweets and movie-this

--- keys.js
+++ keys.js
@@ -1,8 +1,12 @@
-console.log('this is loaded');
-
 exports.twitterKeys = {
-  consumer_key: '	hOZcwu0MWDHG7Wm4E6ZeiaZDL',
-  consumer_secret: 'm9AIwZCURmRpWd2txLUlHZoSwKm75iVQXQLl18NFpJ5AXmTGH',
+  consumer_key: 'hOZcwu0MWDHG7Wm4E6ZeiaZDL',
+  consumer_secret: 'm9AIwZCURmRpWd2txLUlHZoSwKm75iVQXQLl18NFpJ5AXmTGHa',
   access_token_key: '886235530844942337-EP2fKU6pSAANkgUX7f3oEiGNqdaI58R',
-  access_token_secret: '	cdK88it8NdFUyucAJ0zc4KIV8VFXieb8QgR9dezl3POvk',
+  access_token_secret: 'cdK88it8NdFUyucAJ0zc4KIV8VFXieb8QgR9dezl3POvk',
+};
+
+exports.spotifyKeys = {
+  clientId : '944a88ac62cd4876b7df1b7f1252e600',
+  clientSecret : '7981305b3c6d48bea4f9f7fa0b02794b',
+  // redirectUri : 'http://www.example.com/callback'
 };
--- liri.js
+++ liri.js
@@ -1,46 +1,89 @@
 var request = require("request");
-var keys = require("./keys.js");
+var twitter = require("twitter");
+//https://github.com/thelinmichael/spotify-web-api-node
+var spotifyApi = require("node-spotify-api");
 
 var liri = {
   command: "",
   args: [],
+  keys: require("./keys.js"),
 
   run: function(){
-    console.log("START");
+    // console.log("START");
     this.command = process.argv[2].toLowerCase();
     if(process.argv.length >2){
       this.args = process.argv.slice(3, process.argv.length);
     }
-    this.runCommand();
+    this.runCommand(this.command, this.args, false);
 
   },
 
-  runCommand: function(){
-    console.log(this.command);
-    if(this.command === "my-tweets"){
+  runCommand: function(command, args, recurse){
+    console.log(command);
+    if(command === "my-tweets"){
       this.myTweets();
     }
-    if(this.command === "spotify-this-song"){
+    if(command === "spotify-this-song"){
       this.spotifyThisSong();
     }
-    if(this.command === "my-tweets"){
-      this.myTweets();
+    if(command === "movie-this"){
+      this.movieThis(args.join("+"));
     }
-    if(this.command === "my-tweets"){
-      this.myTweets();
+    if(command === "do-what-it-says"){
+      this.doWhatItSays();
     }
   },
 
   myTweets: function(){
-    console.log("Tweets");
+    // console.log("Tweets");
+    var client = new twitter(this.keys.twitterKeys);
+    client.get('statuses/user_timeline', function(error, tweets, response) {
+      if(error){
+        console.log(error);
+        return;
+      }
+
+      // console.log(tweets);  // The favorites.
+      // console.log(response);  // Raw response object.
+      for(var t = 0; t<Math.min(20, tweets.length);t++){
+        console.log(tweets[t].created_at+"\n"+tweets[t].text+"\n");
+      }
+    });
   },
 
-  spotifyThisSong: function(){
+  spotifyThisSong: function(song){
     console.log("spotify");
+
   },
 
-  movieThis: function(){
-    console.log("movie");
+  movieThis: function(movie){
+    // console.log("movie");
+    var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
+    request(queryUrl, function(error, response, body){
+      if(!error && response.statusCode === 200){
+        var movieinfo = JSON.parse(body);
+        console.log("Title: "+movieinfo.Title);
+        console.log("Year: "+movieinfo.Year);
+        var imdbrating, rtrating;
+        for(var r in movieinfo.Ratings){
+          if(r.Source === "Internet Movie Database"){
+            imdbrating = r.Value;
+          }
+          else if(r.Source === "Rotten Tomatoes"){
+            rtrating = r.Value;
+          }
+        }
+        console.log("IMDB Rating: "+imdbrating);
+        console.log("Rotten Tomatoes Rating: "+rtrating);
+        console.log("Country: "+movieinfo.Country);
+        console.log("Language: "+movieinfo.Language);
+        console.log("Plot: "+movieinfo.Plot);
+        console.log("Actors: "+movieinfo.Actors);
+      }
+      else{
+        console.log(error);
+      }
+    });
   },
 
   doWhatItSays: function(){
