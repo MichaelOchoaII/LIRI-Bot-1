a8c48945823071a19f0e7c4a079d1237dc90619d
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-07-16 14:32:26 -0500

    master - adding tests to random

--- liri.js
+++ liri.js
@@ -15,30 +15,40 @@ var liri = {
     if(process.argv.length >2){
       this.args = process.argv.slice(3, process.argv.length).join(" ");
     }
-    this.runCommand(this.command, this.args, false);
+    this.runCommand(this.command, this.args, false, this.printOutput).then(function(output){
+      console.log(output);
+    });
 
   },
 
-  runCommand: function(command, args, recurse){
+  printOutput: function(){
+    console.log(output);
+  },
+
+  runCommand: function(command, args, recurse, callback){
     console.log(command);
-    if(command === "my-tweets"){
-      this.myTweets();
-    }
-    if(command === "spotify-this-song"){
-      this.spotifyThisSong(args);
-    }
-    if(command === "movie-this"){
-      this.movieThis(args.replace(" ", "+"));
-    }
-    if(command === "do-what-it-says"){
-      if(!recurse){
-        this.doWhatItSays();
+    return new Promise(function(resolve, reject){
+      if(command === "my-tweets"){
+        results = liri.myTweets();
       }
-    }
+      if(command === "spotify-this-song"){
+        results = liri.spotifyThisSong(args);
+      }
+      if(command === "movie-this"){
+        results = liri.movieThis(args.replace(" ", "+"));
+      }
+      if(command === "do-what-it-says"){
+        if(!recurse){
+          results = liri.doWhatItSays();
+        }
+      }
+      resolve(results);
+    });
   },
 
   myTweets: function(){
     // console.log("Tweets");
+    var output = "";
     var client = new twitter(this.keys.twitterKeys);
     client.get('statuses/user_timeline', function(error, tweets, response) {
       if(error){
@@ -46,14 +56,18 @@ var liri = {
         return;
       }
       for(var t = 0; t<Math.min(20, tweets.length);t++){
-        console.log(tweets[t].created_at+"\n"+tweets[t].text+"\n");
+        // console.log(tweets[t].created_at+"\n"+tweets[t].text+"\n");
+        output += tweets[t].created_at+"\n"+tweets[t].text+"\n";
       }
+      console.log("TEST"+output)
+      return output;
     });
   },
 
   spotifyThisSong: function(song){
     // console.log("spotify");
     console.log(song);
+    // var output = "";
     var spotify = new spotifyApi(this.keys.spotifyKeys);
     spotify.search({ type: 'track', query: song })
     .then(function(response) {
@@ -67,10 +81,11 @@ var liri = {
         artistlist = artistlist.join(", ");
       }
 
-      console.log(artistlist);
-      console.log(resultTrack.name);
-      console.log(resultTrack.preview_url);
-      console.log(resultTrack.album.name);
+      // console.log(artistlist);
+      // console.log(resultTrack.name);
+      // console.log(resultTrack.preview_url);
+      // console.log(resultTrack.album.name);
+      return artistlist + "\n" + resultTrack.name + "\n" + resultTrack.preview_url + "\n" + resultTrack.album.name;
     })
     .catch(function(err) {
       console.log(error);
@@ -94,12 +109,13 @@ var liri = {
             rtrating = r.Value;
           }
         }
-        console.log("IMDB Rating: "+imdbrating);
-        console.log("Rotten Tomatoes Rating: "+rtrating);
-        console.log("Country: "+movieinfo.Country);
-        console.log("Language: "+movieinfo.Language);
-        console.log("Plot: "+movieinfo.Plot);
-        console.log("Actors: "+movieinfo.Actors);
+        // console.log("IMDB Rating: "+imdbrating);
+        // console.log("Rotten Tomatoes Rating: "+rtrating);
+        // console.log("Country: "+movieinfo.Country);
+        // console.log("Language: "+movieinfo.Language);
+        // console.log("Plot: "+movieinfo.Plot);
+        // console.log("Actors: "+movieinfo.Actors);
+        return "IMDB Rating: "+imdbrating+"\nRotten Tomatoes Rating: "+rtrating+"\nCountry: "+movieinfo.Country+"\nLanguage: "+movieinfo.Language+"\nPlot: "+movieinfo.Plot+"\nActors: "+movieinfo.Actors;
       }
       else{
         console.log(error);
@@ -118,7 +134,7 @@ var liri = {
       var commandlist = data.split("\n");
       for(var c in commandlist){
         var currentCommand = commandlist[c].trim().split(",");
-        liri.runCommand(currentCommand[0], currentCommand[1], true);
+        liri.runCommand(currentCommand[0], currentCommand[1], true, this.printOutput);
       }
     });
   }
